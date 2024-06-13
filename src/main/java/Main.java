7264c4da07c407aa2db4f253f3ccb442de01b813
brunosouza2 <bruno.henrqsouza@gmail.com>
2024-06-13 14:03:24 -0300

    refatorando classe Curso especificando atributos e ajustando classe Main.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Main {
+import br.com.dominio.Curso;
+import br.com.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var curso = new Curso("Especialista Java", "Especialização Java", 60);
+        var mentoria = new Mentoria("Melhorando em Backend",
+                "Subindo na cerreira backend java", LocalDate.now());
+
+        System.out.println(curso);
+        System.out.println(mentoria);
+    }
+
 }
--- src/main/java/br/com/dominio/Curso.java
+++ src/main/java/br/com/dominio/Curso.java
@@ -1,2 +1,33 @@
-package br.com.dominio;public class Curso {
+package br.com.dominio;
+
+public class Curso extends Conteudo {
+
+    private int cargaHoraria;
+
+    public Curso(String titulo, String descricao, int cargaHoraria) {
+        super(titulo, descricao);
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * getCargaHoraria();
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", cargaHoraria=" + getCargaHoraria() +
+                '}';
+    }
 }
