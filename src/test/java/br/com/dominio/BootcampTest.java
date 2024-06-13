3eba355af95c7fe2250111897c4c38da8646dcdf
brunosouza2 <bruno.henrqsouza@gmail.com>
2024-06-13 15:35:06 -0300

    Adicionando testes unitários para a classe Bootcamp

--- src/test/java/br/com/dominio/BootcampTest.java
+++ src/test/java/br/com/dominio/BootcampTest.java
@@ -1,4 +1,46 @@
+package br.com.dominio;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Bootcamp")
 class BootcampTest {
-  
+
+    private Bootcamp bootcamp;
+
+    @BeforeEach
+    public void beforeEach() {
+        bootcamp = new Bootcamp("Backend Java DIO - 2024",
+                "Bootcamp focado no aprendizado da tecnologia java.", LocalDate.now());
+    }
+
+    @Test
+    @DisplayName("Deve inscrever desenvolvedor no bootcamp")
+    void deveInscreverDesenvolvedorNoBootcamp() {
+        var desenvolvedor = new Desenvolvedor("Bruno Souza");
+        var segundoDesenvolvedor = new Desenvolvedor("Luiz Souza");
+
+        Desenvolvedor[] collectionEsperada = new Desenvolvedor[]{
+                desenvolvedor,
+                segundoDesenvolvedor,
+        };
+
+        bootcamp.inscreverDesenvolvedor(desenvolvedor);
+        bootcamp.inscreverDesenvolvedor(segundoDesenvolvedor);
+
+        Desenvolvedor[] desenvolvedoresEscritos = bootcamp.getDesenvolvedoresInscritos().stream().
+                sorted().
+                toList().
+                toArray(new Desenvolvedor[0]);
+
+        assertArrayEquals(collectionEsperada, desenvolvedoresEscritos);
+    }
+
+
+
 }
